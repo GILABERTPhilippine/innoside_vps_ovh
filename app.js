35e0de7aeaa8adf7b9549a3d423a87a0a3cf2ae0
PhilippineG <philippinegilabert@gmail.com>
2018-05-17 14:29:55 +0200

    enregistrement des données dans la bdd

--- app.js
+++ app.js
@@ -19,9 +19,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({
     extended: true
 }));
-app.use(bodyParser.json({
-    type: 'application/*+json'
-}))
+app.use(bodyParser.json({}))
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- routes/index.js
+++ routes/index.js
@@ -1,56 +1,69 @@
 var express = require('express');
 var router = express.Router();
 
-
 //il faut importer path pour l'utiliser
 var path = require('path');
 
 // Connection base de données
 var mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
 mongoose.connect('mongoDB://localhost/philippine');
 
-mongoose.Promise = global.Promise;
 
-var tab;
-
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.collection('fiches').find(function(err, items) {
-    tab = items;
+// création du schéma
+var userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    conf_password: {
+        type: String,
+        // required: true
+    },
+    created_at: {
+        type: Date,
+        default: Date.now
+    }
 });
-db.once('open', function() {
-    // we're connected!
-});
 
+// Utilisation d'un model
+var User = mongoose.model('User', userSchema);
 
 /* GET home page. */
 router.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '../public/index.html'));
 })
 
-router.post('/inscrits', function(req, res) {
-    res.sendFile(path.join(__dirname, '../views/inscrits.html'));
-})
-
 router.post('/subscribe', function(req, res) {
     // on recup les donne envoyer par le formulaire
-    //on creer un objet newUser
-    var newUser = {
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password,
-        confPawword: req.body.confPawword,
-    };
+    var mydata = new User();
+    mydata.username = req.body.username;
+    mydata.email = req.body.email;
+    mydata.password = req.body.password;
+    mydata.confPawword = req.body.conf_password;
 
-    //si tout est ok ici on l'enregistre dans la db
-    // on verra une autre fois le code
-
+    mydata.save()
+        // mydata.save(function(err) {
+        //     if (err) {
+        //         res.send(err);
+        //     }
+        //     res.send({
+        //         message: 'Bravo, les données sont maintenant stockée en base de données'
+        //     });
+        // })
 
     //si l'enregistrement c'est bien passé on renvoie l'acceuil
     // en faite pour que ca marche il faut utiliser path.join dslé :/ 
     res.sendFile(path.join(__dirname, '../public/index.html'));
-    // res.json(newUser)
-    console.log(newUser);
+    console.log(mydata);
 });
 
 
